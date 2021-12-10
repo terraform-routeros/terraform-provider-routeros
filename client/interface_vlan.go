deaa1ae111e1b5b0fbac26adad08539e56dadba9
Guy Newbury <guyn@live.co.uk>
2021-12-10 23:16:56 +0000

    Begin adding interface vlan to client

---
+++ client/interface_vlan.go
@@ -0,0 +1,47 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type VLAN struct {
+	ID                      string `json:".id,omitempty"`
+	Arp                     string `json:"arp,omitempty"`
+	ArpTimeout              string `json:"arp-timeout,omitempty"`
+	Disabled                string `json:"disabled"`
+	Interface               string `json:"interface"`
+	L2Mtu                   string `json:"l2mtu,omitempty"`
+	LoopProtect             string `json:"loop-protect,omitempty"`
+	LoopProtectDisableTime  string `json:"loop-protect-disable-time,omitempty"`
+	LoopProtectSendInterval string `json:"loop-protect-send-interval,omitempty"`
+	LoopProtectStatus       string `json:"loop-protect-status,omitempty"`
+	MacAddress              string `json:"mac-address,omitempty"`
+	Mtu                     string `json:"mtu,omitempty"`
+	Name                    string `json:"name"`
+	Running                 string `json:"running,omitempty"`
+	UseServiceTag           string `json:"use-service-tag,omitempty"`
+	VlanID                  string `json:"vlan-id"`
+}
+
+func (c *Client) CreateVLAN(vlan *VLAN) (*VLAN, error) {
+	reqBody, err := json.Marshal(vlan)
+	if err != nil {
+		return nil, err
+	}
+
+	req, err := http.NewRequest("PUT", fmt.Sprintf("%s/rest/interface/vlan", c.HostURL), bytes.NewBuffer(reqBody))
+
+	if err != nil {
+		return nil, err
+	}
+
+	res := VLAN{}
+	if err := c.sendRequest(req, &res); err != nil {
+		return nil, err
+	}
+
+	return &res, nil
+}
