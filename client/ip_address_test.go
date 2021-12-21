b5029f42c7c1ee48f29649c97242f19a90a7b2c0
Guy Newbury <guyn@live.co.uk>
2021-12-21 11:30:17 +0000

    Fix IP address testing

--- client/ip_address_test.go
+++ client/ip_address_test.go
@@ -6,11 +6,28 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func CreateIpAddressTestObjects() (*IPAddress, error) {
+	c := NewClient(GetCredentialsFromEnvVar())
+	ipaddr := new(IPAddress)
+	ipaddr.Address = "192.168.88.1/24"
+	ipaddr.Interface = "bridge"
+	ipaddr.Network = "192.168.88.0"
+	ipaddr.Disabled = "yes"
+	res, err := c.CreateIPAddress(ipaddr)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
 func TestGetIpAddress(t *testing.T) {
 	c := NewClient(GetCredentialsFromEnvVar())
-	res, err := c.GetIPAddress("*4")
+	ipaddr, err := CreateIpAddressTestObjects()
+	assert.Nil(t, err, "expecting nil error")
+	res, err := c.GetIPAddress(ipaddr.ID)
 	assert.Nil(t, err, "expecting nil error")
 	assert.NotNil(t, res, "expecting non-nil result")
+	assert.Equal(t, res.Address, ipaddr.Address)
 }
 
 func TestCreateIpAddress(t *testing.T) {
@@ -26,7 +43,8 @@ func TestCreateIpAddress(t *testing.T) {
 
 func TestDeleteIpAddress(t *testing.T) {
 	c := NewClient(GetCredentialsFromEnvVar())
-	id := "*60"
-	err := c.DeleteIPAddress(id)
+	ipaddr, err := CreateIpAddressTestObjects()
+	assert.Nil(t, err, "expecting a nil error")
+	err = c.DeleteIPAddress(ipaddr.ID)
 	assert.Nil(t, err, "Expecting a nil error")
 }
