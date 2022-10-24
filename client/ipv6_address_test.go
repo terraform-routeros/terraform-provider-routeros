467c801839831590c0f032e6a2c12fc9e19269ef
Guy Newbury <guyn@live.co.uk>
2022-10-24 19:22:35 +0100

    feature: Add ipv6_address to client, and start adding to provider

---
+++ client/ipv6_address_test.go
@@ -0,0 +1,52 @@
+package client
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func CreateIpv6AddressTestObjects() (*IPv6Address, error) {
+	c := NewClient(GetCredentialsFromEnvVar())
+	ipaddr := new(IPv6Address)
+	ipaddr.Address = "fd55::1/64"
+	ipaddr.Interface = "bridge"
+	ipaddr.Disabled = "yes"
+	res, err := c.CreateIPv6Address(ipaddr)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
+}
+
+func TestGetIpv6Address(t *testing.T) {
+	c := NewClient(GetCredentialsFromEnvVar())
+	ipaddr, err := CreateIpAddressTestObjects()
+	assert.Nil(t, err, "expecting nil error")
+	res, err := c.GetIPAddress(ipaddr.ID)
+	assert.Nil(t, err, "expecting nil error")
+	assert.NotNil(t, res, "expecting non-nil result")
+	assert.Equal(t, res.Address, ipaddr.Address)
+	err = c.DeleteIPv6Address(ipaddr.ID)
+	assert.Nil(t, err, "Expecting a nil error")
+}
+
+func TestCreateIpv6Address(t *testing.T) {
+	c := NewClient(GetCredentialsFromEnvVar())
+	ipaddr := new(IPv6Address)
+	ipaddr.Address = "fd55::2/64"
+	ipaddr.Interface = "bridge"
+	ipaddr.Disabled = "yes"
+	res, err := c.CreateIPv6Address(ipaddr)
+	assert.Nil(t, err, "Expecting a nil error")
+	err = c.DeleteIPv6Address(res.ID)
+	assert.Nil(t, err, "Expecting a nil error")
+}
+
+func TestDeleteIpv6Address(t *testing.T) {
+	c := NewClient(GetCredentialsFromEnvVar())
+	ipaddr, err := CreateIpv6AddressTestObjects()
+	assert.Nil(t, err, "expecting a nil error")
+	err = c.DeleteIPv6Address(ipaddr.ID)
+	assert.Nil(t, err, "Expecting a nil error")
+}
---
+++ routeros/resource_ipv6_address.go
@@ -0,0 +1,180 @@
+package routeros
+
+import (
+	"log"
+	"strconv"
+
+	roscl "github.com/gnewbury1/terraform-provider-routeros/client"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func resourceIPv6Address() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceIPv6AddressCreate,
+		Read:   resourceIPv6AddressRead,
+		Update: resourceIPv6AddressUpdate,
+		Delete: resourceIPv6AddressDelete,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: map[string]*schema.Schema{
+			"actual_interface": {
+				Type:     schema.TypeString,
+				Computed: true,
+			},
+			"address": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"advertise": {
+				Type:     schema.TypeBool,
+				Required: true,
+			},
+			"comment": {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
+			"disabled": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"eui64": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+			"from_pool": {
+				Type:     schema.TypeString,
+				Optional: true,
+				Default:  "",
+			},
+			"interface": {
+				Type:     schema.TypeString,
+				Required: true,
+			},
+			"invalid": {
+				Type:     schema.TypeBool,
+				Computed: true,
+			},
+			"no_dad": {
+				Type:     schema.TypeBool,
+				Optional: true,
+				Default:  false,
+			},
+		},
+	}
+}
+
+func resourceIPv6AddressCreate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*roscl.Client)
+	ip_addr := new(roscl.IPAddress)
+
+	ip_addr.Address = d.Get("address").(string)
+	ip_addr.Comment = d.Get("comment").(string)
+	ip_addr.Disabled = strconv.FormatBool(d.Get("disabled").(bool))
+	ip_addr.Interface = d.Get("interface").(string)
+	ip_addr.Network = d.Get("network").(string)
+	ip_addr.ActualInterface = d.Get("actual_interface").(string)
+	ip_addr.Invalid = strconv.FormatBool(d.Get("invalid").(bool))
+
+	res, err := c.CreateIPAddress(ip_addr)
+	if err != nil {
+		log.Println("[ERROR] An error was encountered while sending a PUT request to the API")
+		log.Fatal(err.Error())
+		return err
+	}
+
+	disabled, _ := strconv.ParseBool(res.Disabled)
+	dynamic, _ := strconv.ParseBool(res.Dynamic)
+	invalid, _ := strconv.ParseBool(res.Invalid)
+
+	d.SetId(res.ID)
+	d.Set("address", res.Address)
+	d.Set("comment", res.Comment)
+	d.Set("disabled", disabled)
+	d.Set("interface", res.Interface)
+	d.Set("network", res.Network)
+	d.Set("actual_interface", res.ActualInterface)
+	d.Set("dynamic", dynamic)
+	d.Set("invalid", invalid)
+	return nil
+}
+
+func resourceIPv6AddressRead(d *schema.ResourceData, m interface{}) error {
+	c := m.(*roscl.Client)
+	res, err := c.GetIPAddress(d.Id())
+
+	if err != nil {
+		log.Println("[ERROR] An error was encountered while sending a GET request to the API")
+		log.Fatal(err.Error())
+		return err
+	}
+
+	disabled, _ := strconv.ParseBool(res.Disabled)
+	dynamic, _ := strconv.ParseBool(res.Dynamic)
+	invalid, _ := strconv.ParseBool(res.Invalid)
+
+	d.SetId(res.ID)
+	d.Set("address", res.Address)
+	d.Set("comment", res.Comment)
+	d.Set("disabled", disabled)
+	d.Set("interface", res.Interface)
+	d.Set("network", res.Network)
+	d.Set("actual_interface", res.ActualInterface)
+	d.Set("dynamic", dynamic)
+	d.Set("invalid", invalid)
+
+	return nil
+
+}
+
+func resourceIPv6AddressUpdate(d *schema.ResourceData, m interface{}) error {
+	c := m.(*roscl.Client)
+	ip_addr := new(roscl.IPAddress)
+
+	ip_addr.Address = d.Get("address").(string)
+	ip_addr.Comment = d.Get("comment").(string)
+	ip_addr.Disabled = strconv.FormatBool(d.Get("disabled").(bool))
+	ip_addr.Interface = d.Get("interface").(string)
+	ip_addr.Network = d.Get("network").(string)
+	ip_addr.ActualInterface = d.Get("actual_interface").(string)
+	ip_addr.Invalid = strconv.FormatBool(d.Get("invalid").(bool))
+
+	res, err := c.UpdateIPAddress(d.Id(), ip_addr)
+
+	if err != nil {
+		log.Println("[ERROR] An error was encountered while sending a PATCH request to the API")
+		log.Fatal(err.Error())
+		return err
+	}
+
+	disabled, _ := strconv.ParseBool(res.Disabled)
+	dynamic, _ := strconv.ParseBool(res.Dynamic)
+	invalid, _ := strconv.ParseBool(res.Invalid)
+
+	d.SetId(res.ID)
+	d.Set("address", res.Address)
+	d.Set("comment", res.Comment)
+	d.Set("disabled", disabled)
+	d.Set("interface", res.Interface)
+	d.Set("network", res.Network)
+	d.Set("actual_interface", res.ActualInterface)
+	d.Set("dynamic", dynamic)
+	d.Set("invalid", invalid)
+	return nil
+}
+
+func resourceIPv6AddressDelete(d *schema.ResourceData, m interface{}) error {
+	c := m.(*roscl.Client)
+	err := c.DeleteIPAddress(d.Id())
+	if err != nil {
+		log.Println("[ERROR] An error was encountered while sending a DELETE request to the API")
+		log.Fatal(err.Error())
+		return err
+	}
+	d.SetId("")
+	return nil
+}
