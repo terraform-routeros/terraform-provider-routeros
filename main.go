8f51217c67c99ae8a0d9165691f95bd6394bc8f9
Vaerh <vaerh@tutanota.com>
2022-12-15 22:10:55 +0300

    Provider debug mode.

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"flag"
+
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 	"github.com/vaerh/terraform-provider-routeros/routeros"
 )
@@ -9,6 +11,15 @@ import (
 //go:generate go run github.com/hashicorp/terraform-plugin-docs/cmd/tfplugindocs
 
 func main() {
+	var debug bool
+
+	// https://github.com/hashicorp/terraform-docs-common/blob/main/website/docs/plugin/debugging.mdx
+	// https://developer.hashicorp.com/terraform/plugin/debugging
+	flag.BoolVar(&debug, "debug", false, "set to true to run the provider with support for debuggers like delve")
+	flag.Parse()
+
 	plugin.Serve(&plugin.ServeOpts{
-		ProviderFunc: routeros.NewProvider})
+		ProviderFunc: routeros.NewProvider,
+		Debug:        debug,
+	})
 }
