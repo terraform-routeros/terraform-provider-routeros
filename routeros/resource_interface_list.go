31d18974eb7e4f1f532a608da18d45b5936f7921
Guy Newbury <guyn@live.co.uk>
2023-02-13 21:46:15 +0000

    feat: Update resource_interface_list

--- routeros/resource_interface_list.go
+++ routeros/resource_interface_list.go
@@ -1,121 +1,45 @@
 package routeros
 
 import (
-	"log"
-	"strconv"
-
-	roscl "github.com/gnewbury1/terraform-provider-routeros/client"
-
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
 func resourceInterfaceList() *schema.Resource {
+	resSchema := map[string]*schema.Schema{
+		MetaResourcePath: PropResourcePath("/interface/list"),
+		MetaId:           PropId(Name),
+		"name": {
+			Type:     schema.TypeString,
+			Required: true,
+		},
+		"include": {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+		"exclude": {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+		"comment": {
+			Type:     schema.TypeString,
+			Optional: true,
+		},
+		"dynamic": {
+			Type:     schema.TypeBool,
+			Computed: true,
+		},
+	}
+
 	return &schema.Resource{
-		Create: resourceInterfaceListCreate,
-		Read:   resourceInterfaceListRead,
-		Update: resourceInterfaceListUpdate,
-		Delete: resourceInterfaceListDelete,
+		CreateContext: DefaultCreate(resSchema),
+		ReadContext:   DefaultRead(resSchema),
+		UpdateContext: DefaultUpdate(resSchema),
+		DeleteContext: DefaultDelete(resSchema),
+
 		Importer: &schema.ResourceImporter{
 			StateContext: schema.ImportStatePassthroughContext,
 		},
 
-		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:     schema.TypeString,
-				Required: true,
-			},
-			"include": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"exclude": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"comment": {
-				Type:     schema.TypeString,
-				Optional: true,
-			},
-			"dynamic": {
-				Type:     schema.TypeBool,
-				Computed: true,
-			},
-		},
+		Schema: resSchema,
 	}
-}
-
-func resourceInterfaceListCreate(d *schema.ResourceData, m interface{}) error {
-
-	c := m.(*roscl.Client)
-	list_obj := new(roscl.InterfaceList)
-
-	list_obj.Name = d.Get("name").(string)
-	list_obj.Include = d.Get("include").(string)
-	list_obj.Exclude = d.Get("exclude").(string)
-	list_obj.Comment = d.Get("comment").(string)
-
-	res, err := c.CreateInterfaceList(list_obj)
-	if err != nil {
-		log.Println("[ERROR] An error was encountered while sending a PUT request to the API")
-		log.Fatal(err.Error())
-		return err
-	}
-
-	d.SetId(res.ID)
-	return resourceInterfaceListRead(d, m)
-}
-
-func resourceInterfaceListRead(d *schema.ResourceData, m interface{}) error {
-	c := m.(*roscl.Client)
-	list, err := c.ReadInterfaceList(d.Id())
-
-	if err != nil {
-		log.Println("[ERROR] An error was encountered while sending a GET request to the API")
-		log.Fatal(err.Error())
-		return err
-	}
-
-	dynamic, _ := strconv.ParseBool(list.Dynamic)
-
-	d.SetId(list.ID)
-	d.Set("name", list.Name)
-	d.Set("include", list.Include)
-	d.Set("dynamic", dynamic)
-	d.Set("exclude", list.Exclude)
-	d.Set("comment", list.Comment)
-	return nil
-
-}
-
-func resourceInterfaceListUpdate(d *schema.ResourceData, m interface{}) error {
-	c := m.(*roscl.Client)
-	list_obj := new(roscl.InterfaceList)
-	list_obj.Name = d.Get("name").(string)
-	list_obj.Include = d.Get("include").(string)
-	list_obj.Exclude = d.Get("exclude").(string)
-	list_obj.Comment = d.Get("comment").(string)
-
-	res, err := c.UpdateInterfaceList(d.Id(), list_obj)
-
-	if err != nil {
-		log.Println("[ERROR] An error was encountered while sending a PATCH request to the API")
-		log.Fatal(err.Error())
-		return err
-	}
-
-	d.SetId(res.ID)
-
-	return nil
-}
-
-func resourceInterfaceListDelete(d *schema.ResourceData, m interface{}) error {
-	c := m.(*roscl.Client)
-	err := c.DeleteInterfaceList(d.Id())
-	if err != nil {
-		log.Println("[ERROR] An error was encountered while sending a DELETE request to the API")
-		log.Fatal(err.Error())
-		return err
-	}
-	d.SetId("")
-	return nil
 }
--- routeros/resource_interface_list_test.go
+++ routeros/resource_interface_list_test.go
@@ -2,32 +2,38 @@ package routeros
 
 import (
 	"fmt"
-	"net/http"
 	"testing"
 
-	"github.com/gnewbury1/terraform-provider-routeros/client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-const testListAddress = "routeros_interface_list.test_list"
-const testListName = "List_TEST"
+const testInterfaceListAddress = "routeros_interface_list.test_list"
+const testInterfaceListName = "List_TEST"
 
 func TestAccInterfaceListTest_basic(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckInterfaceListDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccInterfaceListConfig(),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckInterfaceListExists(testListAddress),
-					resource.TestCheckResourceAttr(testListAddress, "name", testListName),
-				),
-			},
-		},
-	})
+	for _, name := range testNames {
+		t.Run(name, func(t *testing.T) {
+			resource.Test(t, resource.TestCase{
+				PreCheck: func() {
+					testAccPreCheck(t)
+					testSetTransportEnv(t, name)
+				},
+				ProviderFactories: testAccProviderFactories,
+				CheckDestroy:      testCheckResourceDestroy("/interface/list", "routeros_interface_list"),
+				Steps: []resource.TestStep{
+					{
+						Config: testAccInterfaceListConfig(),
+						Check: resource.ComposeTestCheckFunc(
+							testAccCheckInterfaceListExists(testInterfaceListAddress),
+							resource.TestCheckResourceAttr(testInterfaceListAddress, "name", "test_list"),
+						),
+					},
+				},
+			})
+
+		})
+	}
 }
 
 func testAccCheckInterfaceListExists(name string) resource.TestCheckFunc {
@@ -56,31 +62,4 @@ resource "routeros_interface_list" "test_list" {
 	name      = "List_TEST"
 }
 `
-}
-
-func testAccCheckInterfaceListDestroy(s *terraform.State) error {
-	c := testAccProvider.Meta().(*client.Client)
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "routeros_interface_list" {
-			continue
-		}
-		id := rs.Primary.ID
-		req, err := http.NewRequest("GET", fmt.Sprintf("%s/interface/list/%s", c.HostURL, id), nil)
-		if err != nil {
-			return err
-		}
-		req.Header.Set("Content-Type", "application/json")
-		req.SetBasicAuth(c.Username, c.Password)
-
-		res, err := c.HTTPClient.Do(req)
-		if err != nil {
-			return nil
-		}
-		if res.StatusCode != 404 {
-			return fmt.Errorf("list id %s has been found", id)
-		}
-		return nil
-	}
-
-	return nil
 }
