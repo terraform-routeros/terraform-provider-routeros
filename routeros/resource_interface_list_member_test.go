d407a9b73c06a2490507b304218bd7d6751d214c
Guy Newbury <guyn@live.co.uk>
2023-02-13 21:44:14 +0000

    feat: Update resource_interface_list_member

--- routeros/resource_interface_list_member_test.go
+++ routeros/resource_interface_list_member_test.go
@@ -2,32 +2,38 @@ package routeros
 
 import (
 	"fmt"
-	"net/http"
 	"testing"
 
-	"github.com/gnewbury1/terraform-provider-routeros/client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-const testListMemberAddress = "routeros_interface_list_member.test_list_member"
-const testListMemberInterface = "ether1"
+const testInterfaceListMemberAddress = "routeros_interface_list_member.test_list_member"
+const testInterfaceListMemberInterface = "ether1"
 
 func TestAccInterfaceListMemberTest_basic(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckInterfaceListMemberDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccInterfaceListMemberConfig(),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckInterfaceListMemberExists(testListMemberAddress),
-					resource.TestCheckResourceAttr(testListMemberAddress, "interface", "ether1"),
-				),
-			},
-		},
-	})
+	for _, name := range testNames {
+		t.Run(name, func(t *testing.T) {
+			resource.Test(t, resource.TestCase{
+				PreCheck: func() {
+					testAccPreCheck(t)
+					testSetTransportEnv(t, name)
+				},
+				ProviderFactories: testAccProviderFactories,
+				CheckDestroy:      testCheckResourceDestroy("/interface/list/member", "routeros_interface_list_member"),
+				Steps: []resource.TestStep{
+					{
+						Config: testAccInterfaceListMemberConfig(),
+						Check: resource.ComposeTestCheckFunc(
+							testAccCheckInterfaceListMemberExists(testInterfaceListMemberAddress),
+							resource.TestCheckResourceAttr(testInterfaceListMemberAddress, "name", "test_list_member"),
+						),
+					},
+				},
+			})
+
+		})
+	}
 }
 
 func testAccCheckInterfaceListMemberExists(name string) resource.TestCheckFunc {
@@ -57,31 +63,4 @@ resource "routeros_interface_list_member" "test_list_member" {
 	list           = "list"
 }
 `
-}
-
-func testAccCheckInterfaceListMemberDestroy(s *terraform.State) error {
-	c := testAccProvider.Meta().(*client.Client)
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "routeros_interface_list_member" {
-			continue
-		}
-		id := rs.Primary.ID
-		req, err := http.NewRequest("GET", fmt.Sprintf("%s/interface/list_member/member/%s", c.HostURL, id), nil)
-		if err != nil {
-			return err
-		}
-		req.Header.Set("Content-Type", "application/json")
-		req.SetBasicAuth(c.Username, c.Password)
-
-		res, err := c.HTTPClient.Do(req)
-		if err != nil {
-			return nil
-		}
-		if res.StatusCode != 404 {
-			return fmt.Errorf("list_member id %s has been found", id)
-		}
-		return nil
-	}
-
-	return nil
 }
