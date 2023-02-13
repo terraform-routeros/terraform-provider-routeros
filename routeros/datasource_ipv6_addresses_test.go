f1f03172be3067d16e25adb698937d0287f78e9e
Guy Newbury <guyn@live.co.uk>
2023-02-13 20:51:16 +0000

    feat: Update IPv6 address datasource

--- routeros/datasource_ipv6_addresses_test.go
+++ routeros/datasource_ipv6_addresses_test.go
@@ -11,18 +11,25 @@ import (
 const testDatasourceIpv6AddressesAddress = "data.routeros_ipv6_addresses.addresses"
 
 func TestAccDatasourceIpv6AddressesTest_basic(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:  func() { testAccPreCheck(t) },
-		Providers: testAccProviders,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccDatasourceIpv6AddressesConfig(),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckDatasourceIpv6AddressesExists(testDatasourceIpv6AddressesAddress),
-				),
-			},
-		},
-	})
+	for _, name := range testNames {
+		t.Run(name, func(t *testing.T) {
+			resource.Test(t, resource.TestCase{
+				PreCheck: func() {
+					testAccPreCheck(t)
+					testSetTransportEnv(t, name)
+				},
+				ProviderFactories: testAccProviderFactories,
+				Steps: []resource.TestStep{
+					{
+						Config: testAccDatasourceIpv6AddressesConfig(),
+						Check: resource.ComposeTestCheckFunc(
+							testAccCheckDatasourceIpv6AddressesExists(testDatasourceIpv6AddressesAddress),
+						),
+					},
+				},
+			})
+		})
+	}
 }
 
 func testAccCheckDatasourceIpv6AddressesExists(name string) resource.TestCheckFunc {
