7369d795a4080ff54e760dd24546750d503ae724
Guy Newbury <guyn@live.co.uk>
2023-02-13 21:39:01 +0000

    feat: Update routeros_capsman_security

--- routeros/resource_capsman_security_test.go
+++ routeros/resource_capsman_security_test.go
@@ -2,10 +2,8 @@ package routeros
 
 import (
 	"fmt"
-	"net/http"
 	"testing"
 
-	"github.com/gnewbury1/terraform-provider-routeros/client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
@@ -13,20 +11,28 @@ import (
 const testCapsManSecurityAddress = "routeros_capsman_security.test_security"
 
 func TestAccCapsManSecurityTest_basic(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckCapsManSecurityDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccCapsManSecurityConfig(),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckCapsManSecurityExists(testCapsManSecurityAddress),
-					resource.TestCheckResourceAttr(testCapsManSecurityAddress, "name", "test_security"),
-				),
-			},
-		},
-	})
+	for _, name := range testNames {
+		t.Run(name, func(t *testing.T) {
+			resource.Test(t, resource.TestCase{
+				PreCheck: func() {
+					testAccPreCheck(t)
+					testSetTransportEnv(t, name)
+				},
+				ProviderFactories: testAccProviderFactories,
+				CheckDestroy:      testCheckResourceDestroy("/caps-man/security", "routeros_capsman_security"),
+				Steps: []resource.TestStep{
+					{
+						Config: testAccCapsManSecurityConfig(),
+						Check: resource.ComposeTestCheckFunc(
+							testAccCheckCapsManSecurityExists(testCapsManSecurityAddress),
+							resource.TestCheckResourceAttr(testCapsManSecurityAddress, "name", "test_security"),
+						),
+					},
+				},
+			})
+
+		})
+	}
 }
 
 func testAccCheckCapsManSecurityExists(name string) resource.TestCheckFunc {
@@ -56,31 +62,4 @@ resource "routeros_capsman_security" "test_security" {
   }
 
 `
-}
-
-func testAccCheckCapsManSecurityDestroy(s *terraform.State) error {
-	c := testAccProvider.Meta().(*client.Client)
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "routeros_capsman_security" {
-			continue
-		}
-		id := rs.Primary.ID
-		req, err := http.NewRequest("GET", fmt.Sprintf("%s/caps-man/security/%s", c.HostURL, id), nil)
-		if err != nil {
-			return err
-		}
-		req.Header.Set("Content-Type", "application/json")
-		req.SetBasicAuth(c.Username, c.Password)
-
-		res, err := c.HTTPClient.Do(req)
-		if err != nil {
-			return nil
-		}
-		if res.StatusCode != 404 {
-			return fmt.Errorf("capsman security id %s has been found", id)
-		}
-		return nil
-	}
-
-	return nil
 }
