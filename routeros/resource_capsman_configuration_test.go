24fdae78532d4f1835cbe11def5cef6994de34c7
Guy Newbury <guyn@live.co.uk>
2023-02-13 21:16:00 +0000

    feat: Update resource capsman_configuration

--- routeros/resource_capsman_configuration_test.go
+++ routeros/resource_capsman_configuration_test.go
@@ -2,10 +2,8 @@ package routeros
 
 import (
 	"fmt"
-	"net/http"
 	"testing"
 
-	"github.com/gnewbury1/terraform-provider-routeros/client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
@@ -13,20 +11,27 @@ import (
 const testCapsManConfigurationAddress = "routeros_capsman_configuration.test_configuration"
 
 func TestAccCapsManConfigurationTest_basic(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckCapsManConfigurationDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccCapsManConfigurationConfig(),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckCapsManConfigurationExists(testCapsManConfigurationAddress),
-					resource.TestCheckResourceAttr(testCapsManConfigurationAddress, "name", "test_configuration"),
-				),
-			},
-		},
-	})
+	for _, name := range testNames {
+		t.Run(name, func(t *testing.T) {
+			resource.Test(t, resource.TestCase{
+				PreCheck: func() {
+					testAccPreCheck(t)
+					testSetTransportEnv(t, name)
+				},
+				ProviderFactories: testAccProviderFactories,
+				CheckDestroy:      testCheckResourceDestroy("/caps-man/configuration", "routeros_capsman_configuration"),
+				Steps: []resource.TestStep{
+					{
+						Config: testAccCapsManConfigurationConfig(),
+						Check: resource.ComposeTestCheckFunc(
+							testAccCheckCapsManConfigurationExists(testCapsManConfigurationAddress),
+							resource.TestCheckResourceAttr(testCapsManConfigurationAddress, "name", "test_configuration"),
+						),
+					},
+				},
+			})
+		})
+	}
 }
 
 func testAccCheckCapsManConfigurationExists(name string) resource.TestCheckFunc {
@@ -56,31 +61,4 @@ resource "routeros_capsman_configuration" "test_configuration" {
   }
 
 `
-}
-
-func testAccCheckCapsManConfigurationDestroy(s *terraform.State) error {
-	c := testAccProvider.Meta().(*client.Client)
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "routeros_capsman_configuration" {
-			continue
-		}
-		id := rs.Primary.ID
-		req, err := http.NewRequest("GET", fmt.Sprintf("%s/caps-man/configuration/%s", c.HostURL, id), nil)
-		if err != nil {
-			return err
-		}
-		req.Header.Set("Content-Type", "application/json")
-		req.SetBasicAuth(c.Username, c.Password)
-
-		res, err := c.HTTPClient.Do(req)
-		if err != nil {
-			return nil
-		}
-		if res.StatusCode != 404 {
-			return fmt.Errorf("capsman configuration id %s has been found", id)
-		}
-		return nil
-	}
-
-	return nil
 }
