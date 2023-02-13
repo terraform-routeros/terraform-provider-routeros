0e11e9a5207d5794063f722fe3c2cc1aa96e9e4f
Guy Newbury <guyn@live.co.uk>
2023-02-13 21:30:55 +0000

    feat: Update routeros_capsman_manager

--- routeros/resource_capsman_manager_test.go
+++ routeros/resource_capsman_manager_test.go
@@ -1,13 +1,9 @@
 package routeros
 
 import (
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"net/http"
 	"testing"
 
-	"github.com/gnewbury1/terraform-provider-routeros/client"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
@@ -15,20 +11,27 @@ import (
 const testCapsManManagerAddress = "routeros_capsman_manager.test_manager"
 
 func TestAccCapsManManagerTest_basic(t *testing.T) {
-	resource.Test(t, resource.TestCase{
-		PreCheck:     func() { testAccPreCheck(t) },
-		Providers:    testAccProviders,
-		CheckDestroy: testAccCheckCapsManManagerDestroy,
-		Steps: []resource.TestStep{
-			{
-				Config: testAccCapsManManagerConfig(),
-				Check: resource.ComposeTestCheckFunc(
-					testAccCheckCapsManManagerExists(testCapsManManagerAddress),
-					resource.TestCheckResourceAttr(testCapsManManagerAddress, "enabled", "true"),
-				),
-			},
-		},
-	})
+	for _, name := range testNames {
+		t.Run(name, func(t *testing.T) {
+			resource.Test(t, resource.TestCase{
+				PreCheck: func() {
+					testAccPreCheck(t)
+					testSetTransportEnv(t, name)
+				},
+				ProviderFactories: testAccProviderFactories,
+				CheckDestroy:      testCheckResourceDestroy("/caps-man/manager", "routeros_capsman_manager"),
+				Steps: []resource.TestStep{
+					{
+						Config: testAccInterfaceBridgeConfig(),
+						Check: resource.ComposeTestCheckFunc(
+							testAccCheckInterfaceBridgeExists(testCapsManManagerAddress),
+							resource.TestCheckResourceAttr(testCapsManManagerAddress, "name", "test_manager"),
+						),
+					},
+				},
+			})
+		})
+	}
 }
 
 func testAccCheckCapsManManagerExists(address string) resource.TestCheckFunc {
@@ -58,45 +61,4 @@ resource "routeros_capsman_manager" "test_manager" {
   }
 
 `
-}
-
-func testAccCheckCapsManManagerDestroy(s *terraform.State) error {
-	c := testAccProvider.Meta().(*client.Client)
-	for _, rs := range s.RootModule().Resources {
-		if rs.Type != "routeros_capsman_manager" {
-			continue
-		}
-		req, err := http.NewRequest("GET", fmt.Sprintf("%s/rest/caps-man/manager", c.HostURL), nil)
-		if err != nil {
-			return err
-		}
-		req.Header.Set("Content-Type", "application/json")
-		req.SetBasicAuth(c.Username, c.Password)
-
-		res, err := c.HTTPClient.Do(req)
-		if err != nil {
-			return err
-		}
-
-		defer res.Body.Close()
-
-		if res.StatusCode < http.StatusOK || res.StatusCode >= http.StatusBadRequest {
-			return fmt.Errorf("bad status code: %d", res.StatusCode)
-		}
-		body, _ := ioutil.ReadAll(res.Body)
-		var v map[string]interface{}
-		if len(body) != 0 {
-			if err = json.Unmarshal(body, &v); err != nil {
-				return err
-			}
-		}
-
-		if v["enabled"] != "false" {
-			return fmt.Errorf("capsman manager still enabled")
-		}
-
-		return nil
-	}
-
-	return nil
 }
