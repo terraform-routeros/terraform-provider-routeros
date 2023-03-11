0a935cd4affd9effdd8c0e8190415d055e4aafa9
Vaerh <vaerh@tutanota.com>
2023-03-11 16:28:06 +0300

    fix: /ip/dns/static errors when trying to change the resource type Fixes #156

--- routeros/resource_ip_dns_record.go
+++ routeros/resource_ip_dns_record.go
@@ -6,52 +6,102 @@ import (
 )
 
 /*
-  {
-    ".id": "*1",
-    "address": "192.168.88.1",
-    "comment": "defconf",
-    "disabled": "false",
-    "dynamic": "false",
-    "name": "router.lan",
-    "ttl": "1d"
+  {									  {								  {
+    ".id": "*3",					    ".id": "*5",				    ".id": "*4",
+    "address": "127.0.0.1",			    "address": "ff00::1",		    "cname": "ipv4",
+    "address-list": "subdomain",	    "disabled": "false",		    "disabled": "false",
+    "disabled": "false",			    "dynamic": "false",			    "dynamic": "false",
+    "dynamic": "false",				    "name": "ipv6",				    "name": "cname",
+    "match-subdomain": "true",		    "ttl": "1d",				    "ttl": "1d",
+    "name": "ipv4",					    "type": "AAAA"				    "type": "CNAME"
+    "ttl": "1d"						  },							  },
   },
-  {
-	".id": "*2",
-  	"address": "2001:db8:1000::1",
-	"disabled": "false",
-	"dynamic": "false",
-	"name": "ipv6.example.com",
-	"ttl": "1d",
-	"type": "AAAA"}
-  {
-    ".id": "*9",
-    "address": "192.168.88.1",
-    "disabled": "false",
-    "dynamic": "false",
-    "regexp": ".*pool.ntp.org",
-    "ttl": "1d"
-  },
+  {									  {								  {
+    ".id": "*6",					    ".id": "*7",				    ".id": "*8",
+    "disabled": "false",			    "disabled": "true",			    "disabled": "false",
+    "dynamic": "false",				    "dynamic": "false",			    "dynamic": "false",
+    "forward-to": "127.0.0.1",		    "mx-exchange": "127.0.0.1",	    "name": "ns",
+    "name": "fwd",					    "mx-preference": "10",		    "ns": "127.0.0.1",
+    "ttl": "1d",					    "name": "mx",				    "ttl": "1d",
+    "type": "FWD"					    "ttl": "1d",				    "type": "NS"
+  },								    "type": "MX"				  },
+                                      },
+  {									  {								  {
+    ".id": "*9",					    ".id": "*A",				    ".id": "*B",
+    "disabled": "false",			    "disabled": "false",		    "disabled": "false",
+    "dynamic": "false",				    "dynamic": "false",			    "dynamic": "false",
+    "name": "nxdomain",				    "name": "srv",				    "name": "txt",
+    "ttl": "1d",					    "srv-port": "8080",			    "text": "DNSSEC",
+    "type": "NXDOMAIN"				    "srv-priority": "10",		    "ttl": "1d",
+  },								    "srv-target": "127.0.0.1",	    "type": "TXT"
+                                        "srv-weight": "100",		  }
+                                        "ttl": "1d",
+                                        "type": "SRV"
+                                      },
 */
 
-// ResourceDnsRecord https://wiki.mikrotik.com/wiki/Manual:IP/DNS
+// ResourceDnsRecord https://wiki.mikrotik.com/wiki/Manual:IP/DNS https://help.mikrotik.com/docs/display/ROS/DNS
 func ResourceDnsRecord() *schema.Resource {
 	resSchema := map[string]*schema.Schema{
 		MetaResourcePath: PropResourcePath("/ip/dns/static"),
 		MetaId:           PropId(Id),
 
 		"address": {
-			Type:        schema.TypeString,
-			Required:    true,
-			Description: "The A record to be returend from the DNS hostname.",
+			Type:          schema.TypeString,
+			Optional:      true,
+			Description:   "The A record to be returend from the DNS hostname.",
+			ConflictsWith: []string{"cname", "forward_to", "mx_exchange", "ns", "srv_target", "text"},
 		},
-		KeyComment:  PropCommentRw,
+		"address_list": {
+			Type:     schema.TypeString,
+			Optional: true,
+			Description: "Name of the Firewall address list to which address must be dynamically added when some " +
+				"request matches the entry.",
+		},
+		KeyComment: PropCommentRw,
+		"cname": {
+			Type:          schema.TypeString,
+			Optional:      true,
+			Description:   "Alias name for a domain name.",
+			ConflictsWith: []string{"address", "forward_to", "mx_exchange", "ns", "srv_target", "text"},
+		},
 		KeyDisabled: PropDisabledRw,
 		KeyDynamic:  PropDynamicRo,
+		"forward_to": {
+			Type:          schema.TypeString,
+			Optional:      true,
+			Description:   "The IP address of a domain name server to which a particular DNS request must be forwarded.",
+			ConflictsWith: []string{"address", "cname", "mx_exchange", "ns", "srv_target", "text"},
+		},
+		"match_subdomain": {
+			Type:        schema.TypeBool,
+			Optional:    true,
+			Description: "Whether the record will match requests for subdomains.",
+		},
+		"mx_exchange": {
+			Type:          schema.TypeString,
+			Optional:      true,
+			Description:   "The domain name of the MX server.",
+			ConflictsWith: []string{"address", "cname", "forward_to", "ns", "srv_target", "text"},
+		},
+		"mx_preference": {
+			Type:         schema.TypeInt,
+			Optional:     true,
+			Computed:     true,
+			Description:  "Preference of the particular MX record.",
+			RequiredWith: []string{"mx_exchange"},
+		},
 		"name": {
 			Type:         schema.TypeString,
 			Optional:     true,
 			Description:  "The name of the DNS hostname to be created.",
 			ExactlyOneOf: []string{"name", "regexp"},
+		},
+		"ns": {
+			Type:          schema.TypeString,
+			Optional:      true,
+			Description:   "Name of the authoritative domain name server for the particular record.",
+			ConflictsWith: []string{"address", "cname", "forward_to", "mx_exchange", "srv_target", "text"},
 		},
 		"regexp": {
 			Type:     schema.TypeString,
@@ -59,6 +109,40 @@ func ResourceDnsRecord() *schema.Resource {
 			Description: "DNS regexp. Regexp entries are case sensitive, but since DNS requests are not case sensitive, " +
 				"RouterOS converts DNS names to lowercase, you should write regex only with lowercase letters.",
 			ExactlyOneOf: []string{"name", "regexp"},
+		},
+		"srv_port": {
+			Type:         schema.TypeInt,
+			Optional:     true,
+			Computed:     true,
+			Description:  "The TCP or UDP port on which the service is to be found.",
+			ValidateFunc: validation.IntBetween(0, 65535),
+			RequiredWith: []string{"srv_target"},
+		},
+		"srv_priority": {
+			Type:         schema.TypeInt,
+			Optional:     true,
+			Computed:     true,
+			Description:  "Priority of the particular SRV record.",
+			RequiredWith: []string{"srv_target"},
+		},
+		"srv_target": {
+			Type:          schema.TypeString,
+			Optional:      true,
+			Description:   "The canonical hostname of the machine providing the service ends in a dot.",
+			ConflictsWith: []string{"address", "cname", "forward_to", "mx_exchange", "ns", "text"},
+		},
+		"srv_weight": {
+			Type:         schema.TypeString,
+			Optional:     true,
+			Computed:     true,
+			Description:  "Weight of the particular SRC record.",
+			RequiredWith: []string{"srv_target"},
+		},
+		"text": {
+			Type:          schema.TypeString,
+			Optional:      true,
+			Description:   "Textual information about the domain name.",
+			ConflictsWith: []string{"address", "cname", "forward_to", "mx_exchange", "ns", "srv_target"},
 		},
 		"ttl": {
 			Type:        schema.TypeString,
@@ -72,7 +156,7 @@ func ResourceDnsRecord() *schema.Resource {
 			Computed:    true,
 			Description: "Type of the DNS record. Available values are: A, AAAA, CNAME, FWD, MX, NS, NXDOMAIN, SRV, TXT",
 			ValidateFunc: validation.StringInSlice([]string{"A", "AAAA", "CNAME", "FWD", "MX", "NS", "NXDOMAIN",
-				"SRV", "TXT "}, false),
+				"SRV", "TXT"}, false),
 		},
 	}
 	return &schema.Resource{
--- routeros/resource_ip_dns_record_test.go
+++ routeros/resource_ip_dns_record_test.go
@@ -8,8 +8,6 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/terraform"
 )
 
-const testIpDhsServer = "routeros_dns_record.test_dns_record"
-
 func TestAccIpDnsRecordTest_basic(t *testing.T) {
 	for _, name := range testNames {
 		t.Run(name, func(t *testing.T) {
@@ -24,10 +22,54 @@ func TestAccIpDnsRecordTest_basic(t *testing.T) {
 					{
 						Config: testAccIpDnsRecordConfig(),
 						Check: resource.ComposeTestCheckFunc(
-							testAccCheckIpDnsRecordExists(testIpDhsServer),
-							resource.TestCheckResourceAttr(testIpDhsServer, "address", "192.168.88.33"),
-							resource.TestCheckResourceAttr(testIpDhsServer, "name", "resource.tf"),
-							resource.TestCheckResourceAttr(testIpDhsServer, "ttl", "8h"),
+							// A
+							testAccCheckIpDnsRecordExists("routeros_dns_record.test_dns_a"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_a", "name", "ipv4"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_a", "address", "127.0.0.1"),
+							// A regexp
+							testAccCheckIpDnsRecordExists("routeros_dns_record.test_dns_a_regexp"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_a", "address", "127.0.0.1"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_a_regexp", "regexp", "regexp"),
+							// AAAA
+							testAccCheckIpDnsRecordExists("routeros_dns_record.test_dns_aaaa"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_aaaa", "name", "ipv6"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_aaaa", "address", "ff00::1"),
+							// AAAA "type" is Computed
+							testAccCheckIpDnsRecordExists("routeros_dns_record.test_dns_aaaa_type_computed"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_aaaa_type_computed", "name", "ipv6_tc"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_aaaa_type_computed", "address", "ff00::1"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_aaaa_type_computed", "type", "AAAA"),
+							// CNAME
+							testAccCheckIpDnsRecordExists("routeros_dns_record.test_dns_cname"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_cname", "cname", "ipv4"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_cname", "name", "cname"),
+							// FWD
+							testAccCheckIpDnsRecordExists("routeros_dns_record.test_dns_fwd"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_fwd", "name", "fwd"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_fwd", "forward_to", "127.0.0.1"),
+							// MX
+							testAccCheckIpDnsRecordExists("routeros_dns_record.test_dns_mx"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_mx", "name", "mx"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_mx", "mx_exchange", "127.0.0.1"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_mx", "mx_preference", "10"),
+							// NS
+							testAccCheckIpDnsRecordExists("routeros_dns_record.test_dns_ns"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_ns", "name", "ns"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_ns", "ns", "127.0.0.1"),
+							// NXDOMAIN
+							testAccCheckIpDnsRecordExists("routeros_dns_record.test_dns_nxdomain"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_nxdomain", "name", "nxdomain"),
+							// SRV
+							testAccCheckIpDnsRecordExists("routeros_dns_record.test_dns_srv"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_srv", "name", "srv"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_srv", "srv_port", "8080"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_srv", "srv_priority", "10"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_srv", "srv_target", "127.0.0.1"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_srv", "srv_weight", "100"),
+							// TXT
+							testAccCheckIpDnsRecordExists("routeros_dns_record.test_dns_txt"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_txt", "name", "_acme-challenge.yourwebsite.com"),
+							resource.TestCheckResourceAttr("routeros_dns_record.test_dns_txt", "text", "dW6MrI3nBy3eJgYWH3QAg1Cwk_TvjFESOuKo+mp6nm1"),
 						),
 					},
 				},
@@ -59,11 +101,107 @@ provider "routeros" {
 	insecure = true
 }
 
-resource "routeros_dns_record" "test_dns_record" {
-	address 	 = "192.168.88.33"
-	name		 = "resource.tf"
-    ttl			 = "8h"
-  }
+resource "routeros_dns_record" "test_dns_a" {
+	address         = "127.0.0.1"
+	address_list    = "subdomain"
+	disabled        = false
+	match_subdomain = true
+	name            = "ipv4"
+	ttl             = "8m"
+}
 
+resource "routeros_dns_record" "test_dns_a_regexp" {
+	address         = "127.0.0.1"
+	disabled        = true
+	regexp          = "regexp"
+}
+
+resource "routeros_dns_record" "test_dns_aaaa" {
+	address         = "ff00::1"
+	address_list    = "subdomain"
+	disabled        = false
+	match_subdomain = true
+	name            = "ipv6"
+	ttl             = "8m"
+	type            = "AAAA"
+}
+  
+resource "routeros_dns_record" "test_dns_aaaa_type_computed" {
+	address         = "ff00::1"
+	name            = "ipv6_tc"
+}
+  
+resource "routeros_dns_record" "test_dns_cname" {
+	address_list    = "subdomain"
+	cname           = "ipv4"
+	disabled        = false
+	match_subdomain = true
+	name            = "cname"
+	ttl             = "8m"
+	type            = "CNAME"
+}
+  
+resource "routeros_dns_record" "test_dns_fwd" {
+	address_list    = "subdomain"
+	disabled        = false
+	forward_to      = "127.0.0.1"
+	match_subdomain = true
+	name            = "fwd"
+	ttl             = "8m"
+	type            = "FWD"
+}
+  
+resource "routeros_dns_record" "test_dns_mx" {
+	address_list    = "subdomain"
+	disabled        = false
+	match_subdomain = true
+	mx_exchange     = "127.0.0.1"
+	mx_preference   = 10
+	name            = "mx"
+	ttl             = "8m"
+	type            = "MX"
+}
+  
+resource "routeros_dns_record" "test_dns_ns" {
+	address_list    = "subdomain"
+	disabled        = false
+	match_subdomain = true
+	name            = "ns"
+	ns              = "127.0.0.1"
+	ttl             = "8m"
+	type            = "NS"
+}
+  
+resource "routeros_dns_record" "test_dns_nxdomain" {
+	address_list    = "subdomain"
+	disabled        = false
+	match_subdomain = true
+	name            = "nxdomain"
+	ttl             = "8m"
+	type            = "NXDOMAIN"
+}
+  
+resource "routeros_dns_record" "test_dns_srv" {
+	address_list    = "subdomain"
+	disabled        = false
+	match_subdomain = true
+	name            = "srv"
+	srv_port        = 8080
+	srv_priority    = 10
+	srv_target      = "127.0.0.1"
+	srv_weight      = 100
+	ttl             = "8m"
+	type            = "SRV"
+}
+
+resource "routeros_dns_record" "test_dns_txt" {
+	address_list    = "subdomain"
+	disabled        = false
+	match_subdomain = true
+	name            = "_acme-challenge.yourwebsite.com"
+	text            = "dW6MrI3nBy3eJgYWH3QAg1Cwk_TvjFESOuKo+mp6nm1"
+	ttl             = "8m"
+	type            = "TXT"
+}
 `
 }
