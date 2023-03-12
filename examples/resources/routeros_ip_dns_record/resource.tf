6ad70df2840969c19733a65ffd634222774b13c8
Vaerh <vaerh@tutanota.com>
2023-03-12 20:14:20 +0300

    docs: Refresh

--- examples/resources/routeros_ip_dns_record/resource.tf
+++ examples/resources/routeros_ip_dns_record/resource.tf
@@ -1,11 +1,13 @@
 resource "routeros_ip_dns_record" "name_record" {
   name    = "router.lan"
   address = "192.168.88.1"
+  type    = "A"
 }
 
 resource "routeros_ip_dns_record" "regexp_record" {
   regexp  = ".*pool.ntp.org"
   address = "192.168.88.1"
+  type    = "A"
 }
 
 resource "routeros_dns_record" "aaaa_record" {
