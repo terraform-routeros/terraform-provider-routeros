d7c2982297cb3ad5893f5b02846703708377ac3c
vaerh <64400271+vaerh@users.noreply.github.com>
2023-05-01 20:22:29 +0300

    docs: Fix the resource import example (#196)

--- examples/resources/routeros_ip_service/import.sh
+++ examples/resources/routeros_ip_service/import.sh
@@ -1,3 +1,2 @@
-#The ID can be found via API or the terminal
-#The command for the terminal is -> :put [/ip/service get [print show-ids]]
+# Import with the name of the ip service in case of the example use www-ssl
 terraform import routeros_ip_service.www_ssl www-ssl
