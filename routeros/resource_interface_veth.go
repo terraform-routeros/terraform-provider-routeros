a6fdcf80a2af0d2b14b66683fb578160e7555a99
Vaerh <vaerh@tutanota.com>
2023-05-14 21:28:40 +0300

    feat: Support for veth interfaces #206

---
+++ routeros/resource_interface_veth.go
@@ -0,0 +1,56 @@
+package routeros
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/validation"
+)
+
+/*
+  {
+    ".id": "*8",
+    "address": "192.168.100.2/24",
+    "comment": "comment",
+    "disabled": "false",
+    "gateway": "192.168.100.1",
+    "name": "veth1",
+    "running": "true"
+  }
+*/
+
+// https://help.mikrotik.com/docs/display/ROS/Container
+func ResourceInterfaceVeth() *schema.Resource {
+	resSchema := map[string]*schema.Schema{
+		MetaResourcePath: PropResourcePath("/interface/veth"),
+		MetaId:           PropId(Id),
+
+		"address": {
+			Type:         schema.TypeString,
+			Required:     true,
+			Description:  "IP address.",
+			ValidateFunc: validation.IsCIDR,
+		},
+		KeyComment:  PropCommentRw,
+		KeyDisabled: PropDisabledRw,
+		"gateway": {
+			Type:         schema.TypeString,
+			Optional:     true,
+			Description:  "Gateway IP address.",
+			ValidateFunc: validation.IsIPv4Address,
+		},
+		KeyName:    PropName("Interface name."),
+		KeyRunning: PropRunningRo,
+	}
+
+	return &schema.Resource{
+		CreateContext: DefaultCreate(resSchema),
+		ReadContext:   DefaultRead(resSchema),
+		UpdateContext: DefaultUpdate(resSchema),
+		DeleteContext: DefaultDelete(resSchema),
+
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: resSchema,
+	}
+}
