ea39a2bd5a6613a05d1e9b1b6bab2a6ba233f7db
Menno Hamburg <menno@mhamburg.nl>
2023-06-13 14:51:05 +0200

    Revert back changes

--- routeros/provider_schema_helpers.go
+++ routeros/provider_schema_helpers.go
@@ -14,10 +14,10 @@ import (
 
 // All metadata fields must be present in each resource schema, and the field type must be string.
 const (
-	MetaId           = "id"
-	MetaResourcePath = "path"
-	MetaTransformSet = "ts"
-	MetaSkipFields   = "skip"
+	MetaId           = "___id___"
+	MetaResourcePath = "___path___"
+	MetaTransformSet = "___ts___"
+	MetaSkipFields   = "___skip___"
 )
 
 const (
