fb76fc8ba7d58fccafce80de693ba59d59c51463
Vaerh <vaerh@tutanota.com>
2023-06-19 10:50:28 +0300

    docs: Add import examples

---
+++ examples/resources/routeros_routing_ospf_area/import.sh
@@ -0,0 +1,3 @@
+#The ID can be found via API or the terminal
+#The command for the terminal is -> /routing/ospf/area/print show-ids
+terraform import routeros_routing_ospf_area.test_routing_ospf_area "*0"
---
+++ examples/resources/routeros_routing_ospf_interface_template/import.sh
@@ -0,0 +1,3 @@
+#The ID can be found via API or the terminal
+#The command for the terminal is -> /routing/ospf/interface-template/print show-ids
+terraform import routeros_routing_ospf_interface_template.test_routing_ospf_interface_template "*0"
