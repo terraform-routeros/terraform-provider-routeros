d5e8f859f3e2a786f96788f3e110565e2b5c9ea9
Vaerh <vaerh@tutanota.com>
2023-11-06 20:48:11 +0300

    Add logging function with coloring by levels

--- routeros/log.go
+++ routeros/log.go
@@ -2,9 +2,20 @@ package routeros
 
 import (
 	"context"
+	"os"
+
 	"github.com/fatih/color"
 	"github.com/hashicorp/terraform-plugin-log/tflog"
-	"os"
+)
+
+type logLevel byte
+
+const (
+	TRACE logLevel = 1 + iota
+	DEBUG
+	INFO
+	WARN
+	ERROR
 )
 
 // ColorizedDebug Used to display provider log color messages.
@@ -14,4 +25,22 @@ func ColorizedDebug(ctx context.Context, msg string, args ...map[string]interfac
 		color.NoColor = false
 	}
 	tflog.Debug(ctx, color.GreenString(msg), args...)
+}
+
+func ColorizedMessage(ctx context.Context, level logLevel, msg string, args ...map[string]interface{}) {
+	if _, set := os.LookupEnv("ROS_LOG_COLOR"); set {
+		color.NoColor = false
+	}
+	switch level {
+	case TRACE:
+		tflog.Trace(ctx, color.GreenString(msg), args...)
+	case DEBUG:
+		tflog.Debug(ctx, color.GreenString(msg), args...)
+	case INFO:
+		tflog.Info(ctx, color.HiBlueString(msg), args...)
+	case WARN:
+		tflog.Warn(ctx, color.HiRedString(msg), args...)
+	case ERROR:
+		tflog.Error(ctx, color.HiRedString(msg), args...)
+	}
 }
