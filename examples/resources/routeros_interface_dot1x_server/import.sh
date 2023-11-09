05894afc3145a8af232b3eb517c95ac224f2cfe4
Michael Dokolin <m@dokol.in>
2023-11-09 23:19:45 +0100

    feat: Add 802.1X server resource

---
+++ examples/resources/routeros_interface_dot1x_server/import.sh
@@ -0,0 +1,3 @@
+#The ID can be found via API or the terminal
+#The command for the terminal is -> :put [/interface/dot1x/server get [print show-ids]]
+terraform import routeros_interface_dot1x_server.ether2 *1
