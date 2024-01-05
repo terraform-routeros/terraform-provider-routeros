af71bf3d0bdba8dc23a3ce109cdac09add73ee7f
Felix <felix_mail@mail.ru>
2024-01-05 22:42:51 +0300

    test(user): Add tests

---
+++ routeros/resource_system_user_group_test.go
@@ -0,0 +1,45 @@
+package routeros
+
+import (
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+const testUserGroupAddress = "routeros_system_user_group.test"
+
+func TestAccUserGroupTest_basic(t *testing.T) {
+	t.Parallel()
+	for _, name := range testNames {
+		t.Run(name, func(t *testing.T) {
+			resource.Test(t, resource.TestCase{
+				PreCheck: func() {
+					testAccPreCheck(t)
+					testSetTransportEnv(t, name)
+				},
+				ProviderFactories: testAccProviderFactories,
+				CheckDestroy:      testCheckResourceDestroy("/user/group", "routeros_system_user_group"),
+				Steps: []resource.TestStep{
+					{
+						Config: testAccUserGroupConfig(),
+						Check: resource.ComposeTestCheckFunc(
+							testResourcePrimaryInstanceId(testUserGroupAddress),
+							resource.TestCheckResourceAttr(testUserGroupAddress, "name", "terraform"),
+						),
+					},
+				},
+			})
+
+		})
+	}
+}
+
+func testAccUserGroupConfig() string {
+	return providerConfig + `
+
+resource "routeros_system_user_group" "test" {
+	name        = "terraform"
+	policy      = ["api", "!ftp", "!local", "password", "policy", "read", "!reboot", "!rest-api", "!romon", "sensitive", "!sniff", "!ssh", "!telnet", "!test", "!web", "!winbox", "write"]
+}	
+`
+}
---
+++ routeros/resource_system_user_settings_test.go
@@ -0,0 +1,54 @@
+package routeros
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+const testUserSettings = "routeros_system_user_settings.test"
+
+func TestAccUserSettingsTest_basic(t *testing.T) {
+	t.Parallel()
+	for _, name := range testNames {
+		t.Run(name, func(t *testing.T) {
+			resource.Test(t, resource.TestCase{
+				PreCheck: func() {
+					testAccPreCheck(t)
+					testSetTransportEnv(t, name)
+				},
+				ProviderFactories: testAccProviderFactories,
+				Steps: []resource.TestStep{
+					{
+						Config: testAccUserSettingsConfig("2", "5"),
+						Check: resource.ComposeTestCheckFunc(
+							testResourcePrimaryInstanceId(testUserSettings),
+							resource.TestCheckResourceAttr(testUserSettings, "minimum_categories", "2"),
+							resource.TestCheckResourceAttr(testUserSettings, "minimum_password_length", "5"),
+						),
+					},
+					{
+						Config: testAccUserSettingsConfig("0", "0"),
+						Check: resource.ComposeTestCheckFunc(
+							testResourcePrimaryInstanceId(testUserSettings),
+							resource.TestCheckResourceAttr(testUserSettings, "minimum_categories", "0"),
+							resource.TestCheckResourceAttr(testUserSettings, "minimum_password_length", "0"),
+						),
+					},
+				},
+			})
+
+		})
+	}
+}
+
+func testAccUserSettingsConfig(p1, p2 string) string {
+	return fmt.Sprintf(`%v
+
+resource "routeros_system_user_settings" "test" {
+	minimum_categories      = %v
+	minimum_password_length = %v
+}
+`, providerConfig, p1, p2)
+}
