4de3c6bfbfdb0cffccf06d2f5447aa80b8b8d691
Felix <felix_mail@mail.ru>
2024-01-15 22:37:11 +0300

    docs(switch): Add examples

---
+++ examples/resources/routeros_interface_ethernet_switch_vlan/import.sh
@@ -0,0 +1,3 @@
+#The ID can be found via API or the terminal
+#The command for the terminal is -> :put [/interface/ethernet/switch/vlan get [print show-ids]]
+terraform import routeros_interface_ethernet_switch_vlan.test *0
