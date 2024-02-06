68cb99a350261e5350f5bba987b5a42e8b6a637c
Michael Dokolin <dokmic@gmail.com>
2024-02-06 07:55:12 +0100

    feat: Add `routeros_zerotier_controller` resource to manage ZeroTier controller

---
+++ examples/resources/routeros_zerotier_controller/import.sh
@@ -0,0 +1,3 @@
+#The ID can be found via API or the terminal
+#The command for the terminal is -> :put [/zerotier/controller get [print show-ids]]
+terraform import routeros_zerotier_controller.test '*1'
