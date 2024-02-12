6d14390644e946f301d98ec3484da629dd43ed35
Jose Luis Pedrosa <jlpedrosa@gmail.com>
2024-02-12 23:00:01 +0000

    Add example

---
+++ examples/resources/routeros_ipv6_dhcp_client/import.sh
@@ -0,0 +1,3 @@
+#The ID can be found via API or the terminal
+#The command for the terminal is -> :put [/ipv6/dhcp-client/  get [print show-ids]]
+terraform import routeros_ipv6_dhcp_client.inet_provider "*1"
