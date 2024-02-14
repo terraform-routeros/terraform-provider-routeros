17a26d9a5c2f6a92415da688a64074f7bf97e9ce
Koen Smets <koen.smets@gmail.com>
2024-02-14 17:02:35 +0100

    fix(ipv6): Add fields for /ipv6/dhcp-client (#357)
    * fix(ipv6): Add fields for /ipv6/dhcp-client
    
    * prefer the use of AlwaysPresentNotUserProvided to suppress diff
    
    * ensure dhcp_options is a set of strings

--- examples/resources/routeros_ipv6_dhcp_client/resource.tf
+++ examples/resources/routeros_ipv6_dhcp_client/resource.tf
@@ -11,7 +11,7 @@ resource "routeros_ipv6_dhcp_client" "client" {
   pool_name         = "pub-add-pool"
   interface         = "ether1"
   add-default-route = true
-  pool_prefix_length = "64"
+  pool_prefix_length = 64
   request            = ["prefix"]
   interface          = "ether1"
 }
