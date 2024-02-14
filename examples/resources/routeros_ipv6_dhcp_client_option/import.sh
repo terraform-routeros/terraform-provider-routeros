f1da6339adaa600c40557bd7c316758dfa30896b
Koen Smets <koen.smets@gmail.com>
2024-02-14 18:47:55 +0100

    feat(ipv6): Add support for /ipv6/dhcp-client/option

---
+++ examples/resources/routeros_ipv6_dhcp_client_option/import.sh
@@ -0,0 +1,3 @@
+#The ID can be found via API or the terminal
+#The command for the terminal is -> :put [/ipv6/dhcp-client/option get [print show-ids]]
+terraform import routeros_ipv6_dhcp_client_option.option "*0"
---
+++ routeros/resource_ipv6_dhcp_client_option.go
@@ -0,0 +1,40 @@
+package routeros
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// ResourceDhcpClient https://help.mikrotik.com/docs/display/ROS/DHCP#DHCP-DHCPClient
+func ResourceIPv6DhcpClientOption() *schema.Resource {
+	resSchema := map[string]*schema.Schema{
+		MetaResourcePath: PropResourcePath("/ipv6/dhcp-client/option"),
+		MetaId:           PropId(Id),
+		KeyName:          PropName("The name that will be used in dhcp-client."),
+		"code": {
+			Type:        schema.TypeInt,
+			Required:    true,
+			Description: "The dhcp-client option code.",
+		},
+		"value": {
+			Type:        schema.TypeString,
+			Optional:    true,
+			Description: "The dhcp-client option",
+		},
+		"raw_value": {
+			Type:        schema.TypeString,
+			Computed:    true,
+			Description: "raw_value is computed from value.",
+		},
+	}
+	return &schema.Resource{
+		CreateContext: DefaultCreate(resSchema),
+		ReadContext:   DefaultRead(resSchema),
+		UpdateContext: DefaultUpdate(resSchema),
+		DeleteContext: DefaultDelete(resSchema),
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: resSchema,
+	}
+}
