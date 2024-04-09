8fd3da66a671e1e3e6e62a38749fa0b58f3c1595
Vaerh <vaerh@tutanota.com>
2024-04-09 22:25:24 +0300

    feat(upnp): Add UPNP & UPNP Interfaces Closes #412

---
+++ examples/resources/routeros_ip_upnp_interfaces/resource.tf
@@ -0,0 +1,5 @@
+resource "routeros_ip_upnp_interfaces" "test" {
+    interface           = "ether1"
+    type                = "external"
+    forced_external_ip  = "0.0.0.0"
+}
---
+++ routeros/resource_ip_upnp_interfaces.go
@@ -0,0 +1,48 @@
+package routeros
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+// ResourceUPNPInterfaces https://help.mikrotik.com/docs/display/ROS/UPnP
+func ResourceUPNPInterfaces() *schema.Resource {
+	resSchema := map[string]*schema.Schema{
+		MetaResourcePath:   PropResourcePath("/ip/upnp/interfaces"),
+		MetaId:             PropId(Id),
+		MetaSetUnsetFields: PropSetUnsetFields("type"),
+
+		KeyDisabled: PropDisabledRw,
+		KeyDynamic:  PropDynamicRo,
+		"interface": {
+			Type:        schema.TypeString,
+			Required:    true,
+			Description: "Interface name on which uPnP will be running.",
+		},
+		"type": {
+			Type:     schema.TypeString,
+			Optional: true,
+			Description: `UPnP interface type:
+
+			external - the interface a global IP address is assigned to
+			internal - router's local interface the clients are connected to`,
+			DiffSuppressFunc: AlwaysPresentNotUserProvided,
+		},
+		"forced_external_ip": {
+			Type:             schema.TypeString,
+			Optional:         true,
+			Description:      "Allow specifying what public IP to use if the external interface has more than one IP available.",
+			DiffSuppressFunc: AlwaysPresentNotUserProvided,
+		},
+	}
+	return &schema.Resource{
+		CreateContext: DefaultCreate(resSchema),
+		ReadContext:   DefaultRead(resSchema),
+		UpdateContext: DefaultUpdate(resSchema),
+		DeleteContext: DefaultDelete(resSchema),
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+
+		Schema: resSchema,
+	}
+}
