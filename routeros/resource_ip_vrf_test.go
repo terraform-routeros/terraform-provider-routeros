2f66a9860d7264c5b0a69670804442a7711a96b0
Vaerh <vaerh@tutanota.com>
2024-05-07 20:21:32 +0300

    test(vrf): Test with virtual interfaces

--- routeros/resource_ip_vrf_test.go
+++ routeros/resource_ip_vrf_test.go
@@ -35,11 +35,19 @@ func TestAccIpVrfTest_basic(t *testing.T) {
 
 func testAccIpVrfConfig() string {
 	return providerConfig + `
+resource "routeros_interface_veth" "veth1" {
+	name    = "veth1"
+}
+
+resource "routeros_interface_veth" "veth2" {
+	name    = "veth2"
+}
 
 resource "routeros_ip_vrf" "test_vrf_a" {
 	disabled 	= true
 	name 		= "vrf_1"
-	interfaces 	= ["ether1", "ether2"]
+	interfaces 	= ["veth1", "veth2"]
+	depends_on  = [routeros_interface_veth.veth1, routeros_interface_veth.veth2]
 }
 `
 }
