e7620935df6a827a149e175d4f08d11454d797f7
Vaerh <vaerh@tutanota.com>
2024-06-27 23:44:15 +0300

    docs(routeros_snmp_community): Addresses Doesn't Accept Multiple Addresses #495

--- examples/resources/routeros_snmp_community/resource.tf
+++ examples/resources/routeros_snmp_community/resource.tf
@@ -1,12 +1,17 @@
 resource "routeros_snmp_community" "test" {
-	authentication_password = "authpasswd"
-	authentication_protocol = "MD5"
-	comment                 = "Comment"
-	disabled                = true
-	encryption_password     = "encpassword"
-	encryption_protocol     = "DES"
-	name                    = "private"
-	read_access             = true
-	security                = "private"
-	write_access            = true
+  authentication_password = "authpasswd"
+  authentication_protocol = "MD5"
+  comment                 = "Comment"
+  disabled                = true
+  encryption_password     = "encpassword"
+  encryption_protocol     = "DES"
+  name                    = "private"
+  read_access             = true
+  security                = "private"
+  write_access            = true
+}
+
+resource "routeros_snmp_community" "mything" {
+  addresses = ["10.0.1.12", "10.10.0.129"]
+  name      = "mything"
 }
