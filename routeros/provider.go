25123687e61b76694820b28dea0cbf52a6a28f18
Vaerh <vaerh@tutanota.com>
2024-09-24 14:31:17 +0300

    feat(hotspot): Add new resource `routeros_ip_hotspot_ip_binding`

--- routeros/provider.go
+++ routeros/provider.go
@@ -99,6 +99,7 @@ func Provider() *schema.Provider {
 			"routeros_ip_firewall_mangle":              ResourceIPFirewallMangle(),
 			"routeros_ip_firewall_nat":                 ResourceIPFirewallNat(),
 			"routeros_ip_firewall_raw":                 ResourceIPFirewallRaw(),
+			"routeros_ip_hotspot_ip_binding":           ResourceIpHotspotIpBinding(),
 			"routeros_ip_hotspot_service_port":         ResourceIpHotspotServicePort(),
 			"routeros_ip_hotspot_walled_garden":        ResourceIpHotspotWalledGarden(),
 			"routeros_ip_hotspot_walled_garden_ip":     ResourceIpHotspotWalledGardenIp(),
---
+++ routeros/resource_ip_hotspot_ip_binding_test.go
@@ -0,0 +1,51 @@
+package routeros
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+)
+
+const testIpHotspotIpBinding = "routeros_ip_hotspot_ip_binding.test"
+
+func TestAccIpHotspotIpBindingTest_basic(t *testing.T) {
+	t.Parallel()
+	for _, name := range testNames {
+		t.Run(name, func(t *testing.T) {
+			resource.Test(t, resource.TestCase{
+				PreCheck: func() {
+					testAccPreCheck(t)
+					testSetTransportEnv(t, name)
+				},
+				ProviderFactories: testAccProviderFactories,
+				CheckDestroy:      testCheckResourceDestroy("/ip/hotspot/ip-binding", "routeros_ip_hotspot_ip_binding"),
+				Steps: []resource.TestStep{
+					{
+						Config: testAccIpHotspotIpBindingConfig(),
+						Check: resource.ComposeTestCheckFunc(
+							testResourcePrimaryInstanceId(testIpHotspotIpBinding),
+							resource.TestCheckResourceAttr(testIpHotspotIpBinding, "address", "0.0.0.1"),
+							resource.TestCheckResourceAttr(testIpHotspotIpBinding, "comment", "comment"),
+							resource.TestCheckResourceAttr(testIpHotspotIpBinding, "mac_address", "00:00:00:00:01:10"),
+							resource.TestCheckResourceAttr(testIpHotspotIpBinding, "to_address", "0.0.0.2"),
+						),
+					},
+				},
+			})
+
+		})
+	}
+}
+
+func testAccIpHotspotIpBindingConfig() string {
+	return fmt.Sprintf(`%v
+
+resource "routeros_ip_hotspot_ip_binding" "test" {
+   address     = "0.0.0.1"
+   comment     = "comment"
+   mac_address = "00:00:00:00:01:10"
+   to_address  = "0.0.0.2" 
+}
+`, providerConfig)
+}
