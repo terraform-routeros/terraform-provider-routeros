73823e50db10cab5570d5dfac7d7fe1da7bee5d5
Vaerh <vaerh@tutanota.com>
2025-03-12 21:10:17 +0300

    fix(ethernet): Ethernet `advertise` do not allow multiple values Fixes #692

--- routeros/resource_interface_ethernet.go
+++ routeros/resource_interface_ethernet.go
@@ -72,10 +72,10 @@ func ResourceInterfaceEthernet() *schema.Resource {
 				Advertised speed and duplex modes for Ethernet interfaces over twisted pair, 
 				only applies when auto-negotiation is enabled. Advertising higher speeds than 
 				the actual interface supported speed will have no effect, multiple options are allowed.`,
-			ValidateFunc: validation.StringMatch(
-				regexp.MustCompile(`^[0-9\.]+(M|G)-(full|half|base\w+(-\w+)?)$`),
-				"Since RouterOS v7.12 the values of this property have changed. Please check the documentation.",
-			),
+			// ValidateFunc: validation.StringMatch(
+			// 	regexp.MustCompile(`^[0-9\.]+(M|G)-(full|half|base\w+(-\w+)?)$`),
+			// 	"Since RouterOS v7.12 the values of this property have changed. Please check the documentation.",
+			// ),
 			DiffSuppressFunc: AlwaysPresentNotUserProvided,
 		},
 		KeyArp:        PropArpRw,
