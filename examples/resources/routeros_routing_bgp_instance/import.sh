2e3a4224972d63436435677cec9d48658e75de2d
Vaerh <vaerh@tutanota.com>
2025-12-03 16:50:30 +0300

    docs(import): Fix examples

--- examples/resources/routeros_routing_bgp_instance/import.sh
+++ examples/resources/routeros_routing_bgp_instance/import.sh
@@ -2,6 +2,4 @@
 #The command for the terminal is -> :put [/routing/bgp/instance get [print show-ids]]
 terraform import routeros_routing_bgp_instance.test *3
 #Or you can import a resource using one of its attributes
-terraform import routeros_routing_bgp_instance.test "name=xxx"#Or you can import a resource using one of its attributes
-terraform import routeros_routing_bgp_instance.test
-routeros_routing_bgp_instance.test "name=xxx"
+terraform import routeros_routing_bgp_instance.test "name=xxx"
