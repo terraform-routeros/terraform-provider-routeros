8ba3431aa86fef408952a8423239dc3c28836462
Daniel Nachtrub <dn@nuvotex.de>
2026-03-16 13:45:35 +0100

    refactor: remove deprecated bsd_syslog attribute from system logging action

--- routeros/resource_system_logging_actions_test.go
+++ routeros/resource_system_logging_actions_test.go
@@ -26,7 +26,6 @@ func TestAccSystemLoggingActionTest_basic(t *testing.T) {
 							resource.TestCheckResourceAttr(testSystemLoggingAction, "default", "false"),
 							resource.TestCheckResourceAttr(testSystemLoggingAction, "target", "remote"),
 							resource.TestCheckResourceAttr(testSystemLoggingAction, "remote", "192.168.1.1"),
-							resource.TestCheckResourceAttr(testSystemLoggingAction, "bsd_syslog", "true"),
 							resource.TestCheckResourceAttr(testSystemLoggingAction, "syslog_facility", "user"),
 							resource.TestCheckResourceAttr(testSystemLoggingAction, "syslog_severity", "notice"),
 							resource.TestCheckResourceAttr(testSystemLoggingAction, "syslog_time_format", "iso8601"),
@@ -45,7 +44,6 @@ resource "routeros_system_logging_action" "action" {
 	name               = "action1"
     target             = "remote"
 	remote             = "192.168.1.1"
-    bsd_syslog         = true
 	syslog_facility    = "user"
 	syslog_severity    = "notice"
 	syslog_time_format = "iso8601"
