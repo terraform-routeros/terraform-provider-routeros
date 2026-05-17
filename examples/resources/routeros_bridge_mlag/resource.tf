a65828207e4edfa6b258531ea95c71b77ea58edc
Levi Zitting <me@levizitting.com>
2026-05-17 16:00:05 -0500

    docs: clarify MLAG migration examples

--- examples/resources/routeros_bridge_mlag/resource.tf
+++ examples/resources/routeros_bridge_mlag/resource.tf
@@ -1,3 +1,5 @@
+# Legacy MLAG resource for RouterOS versions before 7.22.
+# RouterOS 7.22+ manages MLAG with routeros_interface_bridge.mlag_* attributes.
 resource "routeros_bridge_mlag" "mlag" {
   bridge    = "bridge1"
   peer_port = "stack-link"
